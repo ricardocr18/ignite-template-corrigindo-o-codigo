0e3c1536904713a73a2962cce695a679d2f24c81
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-07-26 16:46:52 -0300

    atualizando a rota Delete

--- src/index.js
+++ src/index.js
@@ -57,9 +57,9 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -79,7 +79,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({likes});
 });
 
 module.exports = app;
